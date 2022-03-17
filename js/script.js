d44cc85def44b57ba65d888e44396f2601746e7b
Pablo <phf-vieira@hotmail.com>
2022-03-17 17:02:02 -0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
-
-jQuery(function($) {
+jQuery(function ($) {
   $(".menu_dropdown").hide(300);
   $("#genres").mouseover(function () {
     $(".menu_dropdown").show(300);
   });
   $("#genres").mouseleave(function () {
     $(".menu_dropdown").hide(300);
-
   });
-  $('.wrapper_items').slick({
+  $(".animation").hide();
+
+  let waiting_time_one;
+  let waiting_time_two;
+  function show(id) {
+    $(id).show();
+    console.log("show")
+  };
+
+  $("#movie_one").mouseover(function () {
+
+    clearTimeout(waiting_time_one);
+    waiting_time_one = setTimeout(()=> show("#animation_one"), 800);
+    console.log("test")
+    
+  });
+  $("#movie_one").mouseleave(function () {
+    clearTimeout(waiting_time_one);
+    $("#animation_one").hide();
+
+    console.log("testout");
+  });
+  $("#movie_two").mouseover(function () {
+    clearTimeout(waiting_time_two);
+    waiting_time_two = setTimeout(()=> show("#animation_two"), 800);
+  });
+  $("#movie_two").mouseleave(function(){
+    clearTimeout(waiting_time_two);
+    $("#animation_two").hide();
+  });
+
+
+
+  $(".wrapper_items").slick({
     infinite: true,
     speed: 500,
     slidesToShow: 5,
     slidesToScroll: 1,
   });
 });
-
